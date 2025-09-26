b4d5668ea6a156a90d3a0272ec3f656ce3d8134a
Andras-corda <andrasgamer568@gmail.com>
2025-09-26 13:30:42 +0200

    001

---
+++ python-001/main.py
@@ -0,0 +1,17 @@
+
+# globalVariables
+prenom = input("Votre prénom : ") 
+age = int(input("Votre age : "))
+
+print(f"Bonjours {prenom}, vous avez {age} ans")
+print(f"Dans 2 ans vous aurez {age + 2}")
+r = True
+while r:
+    try:
+        prenom = input("Votre prénom : ") 
+        r = False
+        
+    except ValueError:
+        print(f"Erreur, merci de recommencer")
+    
+
